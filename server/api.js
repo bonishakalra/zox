5a2a871e4401db1fcb0adbcb84f3541d0411ffd4
bonishakalra <37516663+bonishakalra@users.noreply.github.com>
2018-04-15 03:15:02 +0530

    changed environment variables

--- server/api.js
+++ server/api.js
@@ -4,11 +4,18 @@ require('dotenv').load();
 module.exports = function(router){
 	
 	/* creating Twitter object conatining all required enviroment variables */
-	var twitter = new Twitter({
+	/*var twitter = new Twitter({
 	  consumer_key: process.env.CONSUMER_KEY,
 	  consumer_secret: process.env.CONSUMER_SECRET,
 	  access_token_key: process.env.ACCESS_TOKEN_KEY,
 	  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+	});*/
+
+	var twitter = new Twitter({
+	  consumer_key: 'BRtUOpFKgTJwLGs9OyfIDPzro',
+	  consumer_secret: 'rD7PzcJse1Gzu0aUkTE0w2xgplZr0EebQXRNnXuXuSBBJHWm0o',
+	  access_token_key: '984527088093822976-VPvF0FO0NTGM41V4oll3UOLwdWYF8K0',
+	  access_token_secret: 'zVmRdXqpEPs89SqWqtpblVM9kQtcEf8PRokC8FKpTRvm2'
 	});
 
 	var screenName = ''; 
